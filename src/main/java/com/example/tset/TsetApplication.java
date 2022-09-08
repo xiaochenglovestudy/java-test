228e294bfd367fe5aa0b282fe3fbe81ff6c64d48
panxiaocheng <1945516372@qq.com>
2022-09-08 20:33:27 +0800

    moban

---
+++ src/main/java/com/example/tset/TsetApplication.java
@@ -0,0 +1,15 @@
+package com.example.tset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsetApplication.class, args);
+    }
+
+
+
+}
